35c74b623a3ef1a6b3b9d16554d33f0648987f17
diyormahmudov <133942826+diyormahmudov@users.noreply.github.com>
2023-05-23 10:24:23 +0600

    food

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let div = document.querySelector("div")
+
+// div.classList.add('box')   присваевает класс 
+// div.classList.remove('box')   уделяет класс
+
+const btnClick =() =>{
+    div.classList.toggle('box')
+}
